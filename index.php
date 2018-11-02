683bf24bc268ed8895d6e8899123cbaba30a76bd
欣玲 <103306022@nccu.edu.tw>
2018-11-02 15:09:25 +0800

    test

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$mysqli = new mysqli("localhost","root","", "datamodel");
+$mysqli->query("SET NAMES utf8");
+$data = $mysqli -> query("SELECT * FROM department")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>DM</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+</head>
+<body>
+  <div class="container">
+    <h1>部門</h1>
+
+  <br><br>
+    <table class="table table-striped">
+      <tr>
+        <td>ID</td>
+        <td>名稱</td>
+
+      </tr>
+      <?php
+      for($i=1;$i<=$data->num_row();$i++){
+         $rs=$data->fetch_row();
+      ?>
+       <tr>
+        <td><?php echo $rs[0];?></td>
+        <td><?php echo $rs[1];?></td>
+
+      </tr>
+      <?php
+    }
+    ?>
+  </table>
+  </div>
+</body>
+</html>
