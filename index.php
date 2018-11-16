c83ee4ef118f4b4be917ad186bd4a476d56deef2
欣玲 <103306022@nccu.edu.tw>
2018-11-16 22:11:10 +0800

    11/16 by Yoda: Create file yoda.php for test

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+/*<?php 
+	header("Content-Type: text/html; charset=utf-8");
+	$db_link = mysqli_connect("localhost", "root", "datamodel");
+	if (!$db_link) {
+		die(  "資料連結失敗");
+	}else{
+		echo "資料連結成功";
+	}
+?>*/
+
 <?php
 $mysqli = new mysqli("localhost","root","", "datamodel");
 $mysqli->query("SET NAMES utf8");
