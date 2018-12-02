51e9365cc853a7bf713a3f33b4bb88b4a70187a6
Alice Tsai <alice90270@gmail.com>
2018-12-02 18:55:08 +0800

    Courses Bought page
    add SQL Query

--- Alice/coursesBought.php
+++ Alice/coursesBought.php
@@ -5,14 +5,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Courses Taken</title>
+  <title>Courses Bought</title>
   <meta charset="utf-8">
 </head>
 
 <body>
-<h1>Courses Taken</h1>
+<h1>Courses Bought</h1>
 <!-- Display table data. -->
 <table border="1" cellpadding="2" cellspacing="2">
+	<tr>
+		<td>Course Type</td>
+		<td>Price</td>
+		<td>Total Number</td>
+		<td>Remaining Number</td>
+	</tr>
+		<?php
 
+		$result = mysqli_query($mysqli, "SELECT `Course_Type`,`Price`, `Number_of_Session`,`Remaining_Number`
+		FROM `course`
+		WHERE `course`.`Course_ID`=1
+		"); 
+		while($query_data = mysqli_fetch_row($result)) {
+		  echo "<tr>";
+		  echo "<td>",$query_data[0], "</td>",
+			   "<td>",$query_data[1], "</td>",
+			   "<td>",$query_data[2], "</td>",
+			   "<td>",$query_data[3], "</td>";
+		  echo "</tr>";
+		}
+		?>
+	
 </body>
 </html>
