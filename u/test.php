dde43c57e656c8044f9807a3014e6321a323ebc9
Alice Tsai <alice90270@gmail.com>
2018-12-08 21:46:47 +0800

    update test.php

--- u/test.php
+++ u/test.php
@@ -3,6 +3,7 @@ $username = "1";
 $password = "1234";
 
 if ($_POST['username'] == $username && $_POST['password'] == $password ) {
+	echo '<meta http-equiv=REFRESH CONTENT=1;url=Member/coursesBought.php>';
 }
 else {
 	echo '<meta http-equiv=REFRESH CONTENT=1;url=/Instructor/Instructor_Center.php>';	
