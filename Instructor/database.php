b6a92daa9e0c064327e612eba45c79296b5c67bc
Yoda <ayuttyua@gmail.com>
2018-12-10 09:57:53 +0800

    DATABASE ADD aws

--- Instructor/database.php
+++ Instructor/database.php
@@ -6,7 +6,19 @@
 	$mysqli= new mysqli($mysqlhost,$mysqluser,$mysqlpassword,$mysqldb);
 	//錯誤處理
 	if ($mysqli->connect_error != "") {
-		echo "資料庫連結失敗！";
+		echo "本機資料庫連結失敗，嘗試連線線上資料庫！";
+
+		$mysqlhost = "project2-2.cr44rp6foiyg.ap-southeast-1.rds.amazonaws.com";
+		$mysqluser = "MyADSL";
+		$mysqlpassword = "d.m._MADSL";
+		$mysqldb = "MADSL_GYM";
+		if ($mysqli->connect_error != "") {
+			echo "線上資料庫連結失敗！";
+		}else{
+			//設定字元集與編碼
+			$mysqli->query("set names 'utf8';");
+			date_default_timezone_set("Asia/Taipei");
+		}
 	}else{
 		//設定字元集與編碼
 		$mysqli->query("set names 'utf8';");
--- Member/database.php
+++ Member/database.php
@@ -6,5 +6,34 @@
 	$mysqli= new mysqli($mysqlhost,$mysqluser,$mysqlpassword,$mysqldb);
 	$mysqli->query("set names 'utf8';");
 	date_default_timezone_set("Asia/Taipei");
+?>
+<?php
+	$mysqlhost = "localhost";
+	$mysqluser = "root";
+	$mysqlpassword = "";
+	$mysqldb = "myadsl_gym";
+	$mysqli= new mysqli($mysqlhost,$mysqluser,$mysqlpassword,$mysqldb);
+	//錯誤處理
+	if ($mysqli->connect_error != "") {
+		echo "本機資料庫連結失敗，嘗試連線線上資料庫！";
+
+		$mysqlhost = "project2-2.cr44rp6foiyg.ap-southeast-1.rds.amazonaws.com";
+		$mysqluser = "MyADSL";
+		$mysqlpassword = "d.m._MADSL";
+		$mysqldb = "MADSL_GYM";
+		if ($mysqli->connect_error != "") {
+			echo "線上資料庫連結失敗！";
+		}else{
+			//設定字元集與編碼
+			$mysqli->query("set names 'utf8';");
+			date_default_timezone_set("Asia/Taipei");
+		}
+		
+	}else{
+		//設定字元集與編碼
+		$mysqli->query("set names 'utf8';");
+		date_default_timezone_set("Asia/Taipei");
+	}
 	session_start();
 ?>
+
