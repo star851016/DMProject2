fed91733e8138b0a1dd4da3ab57f746a1e13f013
Alice Tsai <alice90270@gmail.com>
2018-12-10 23:17:08 +0800

    delete old code

--- u/test.php
+++ u/test.php
@@ -59,19 +59,4 @@ else{
 		}
 }
 
-
-
-
-
-
-
-
-
-if ($_POST['username'] == $username && $_POST['password'] == $password ) {
-	echo '<meta http-equiv=REFRESH CONTENT=1;url=../Member/coursesBought.php>';
-}
-else {
-	echo '<meta http-equiv=REFRESH CONTENT=1;url=../Instructor/Instructor_Center.php>';	
-}
-
 ?>
